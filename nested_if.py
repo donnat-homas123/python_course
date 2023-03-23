64e2ab84013f5bac9bbc9c50749b9d09789df67d
DONNA THOMAS <91842015+donnat-homas123@users.noreply.github.com>
2023-03-23 22:43:53 +0530

    Delete nested_if.py

--- nested_if.py
+++
@@ -1,9 +0,0 @@
-mark=int(input('enter marks'))
-if mark>27:
-    print('passed')
-    if mark>90 :
-        print('u got A+')
-    else:
-        print('u dont have an A+')
-else:
-    print('u failed')       
