7cc3f15e3aa6b5af372057c2343ae29bc166bb5c
DONNA THOMAS <91842015+donnat-homas123@users.noreply.github.com>
2023-03-23 22:47:34 +0530

    Add files via upload

---
+++ day1/su_m.py
@@ -0,0 +1,13 @@
+x=int(input('enter a number '))
+y=int(input('enter a number '))
+print(x+y)
+
+print('taking another 2 number a and b')
+a=10.54
+b=12
+sum=a+b
+print(type(a))
+print(type(b))
+print(type(sum))
+print(sum)
+print(int(sum))
