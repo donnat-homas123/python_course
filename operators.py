ce97b302b0b7e186bdf30ce42afa34b3d2794e0c
DONNA THOMAS <91842015+donnat-homas123@users.noreply.github.com>
2023-03-23 22:43:44 +0530

    Delete operators.py

--- operators.py
+++
@@ -1,9 +0,0 @@
-x=10;y=15;z=10;a=3;b=4
-print('equals',x==z)
-print('equals',x==y)
-print('less than or equal',a<=b)
-print('greater than or equal',y>=a)
-print('not equal to',x !=z)
-print('not equal to',x !=y)
-print('less than',a<b)
-print('greater than',y>a)
